43c63102b12373a70b2ecc400c1a77bda89a917f
jjoc <director@ocpsistemas.eu>
2014-02-25 12:57:40 +0100

    4Installing:  FQDN+Key

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,52 +1,52 @@
 $site_name = 'Agora Voting'
-$agora_fqdn = 'local.dev'
-$agora_ssl_port = '9443'
-$fnmt_fqdn = 'fnmt.local.dev'
-$session_cookie_domain = '.local.dev'
+$agora_fqdn = 'ocptecnicos.net'
+$agora_ssl_port = '443'
+$fnmt_fqdn = 'fnmt.ocptecnicos.net'
+$session_cookie_domain = 'cooks.ocptecnicos.net'
 
 # if activated, it will overwrite agora settings if present
 $overwrite_agora_settings = 'true'
 
 $agora_auto_join = ''
-$agora_admin_username = 'agora'
-$agora_admin_password = 'some password'
+$agora_admin_username = 'ipirates'
+$agora_admin_password = '_DIPiPirates2014_'
 $agora_front_page = 'None'
 
 $enable_varnish = 'true'
 
 # email stuff (used in multiple places like agora and sentry..)
 
-$server_mail = 'agora@local.dev'
-$email_host = ''
-$email_host_password = ''
-$email_host_user = ''
-$email_port = ''
+#$server_mail = ''
+#$email_host = ''
+#$email_host_password = ''
+#$email_host_user = ''
+#$email_port = ''
 
-$agora_auto_activation_secret = 'some secret key'
+$agora_auto_activation_secret = '_DIPiPirates2014_'
 
-$db_password =  'some secret password'
+$db_password =  '_DIPiPirates2014_'
 
-$django_secret_key = 'some secret password'
+$django_secret_key = '_DIPiPirates2014_'
 
 # admin and ssh stuff
 
-$admin_name = 'Bob Esponja'
-$admin_email = 'bob@local.dev'
+$admin_name = 'John Smith'
+$admin_email = 'sindic@citizenship.es'
 $ssh_authorized_keys = 'ssh-dss ea:43:54:fa:90:3b:ab:74:7d:27:91:fa:7b:db:3b:fc root@iTodoRedGOB'
 
 # sentry conf
 
-$sentry_ssl_port = '9443'
-$sentry_fqdn = 'sentry.local.dev'
-$sentry_secret_key = 'some secret password'
-$sentry_db_password =  'some secret'
+$sentry_ssl_port = '443'
+$sentry_fqdn = 'sentry.ocptecnicos.net'
+$sentry_secret_key = '_DIPiPirates2014'
+$sentry_db_password =  '_DIPiPirates2014_'
 
 $sentry_username = 'admin'
-$sentry_userpass = 'admin pass'
+$sentry_userpass = '_DIPiPirates2014_'
 
 # backup
 
-$backup_password = 'backup password'
+$backup_password = '_DIPiPirates2014_'
 
 require agora
 require sentry
