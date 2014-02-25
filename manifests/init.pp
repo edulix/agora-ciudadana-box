80b2b8902d2e97066ce7b6164b5e2deb367d71b1
jjoc <director@ocpsistemas.eu>
2014-02-25 11:46:32 +0100

    With eigen KEYs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,3 @@
-
 $site_name = 'Agora Voting'
 $agora_fqdn = 'local.dev'
 $agora_ssl_port = '9443'
@@ -33,7 +32,7 @@ $django_secret_key = 'some secret password'
 
 $admin_name = 'Bob Esponja'
 $admin_email = 'bob@local.dev'
-$ssh_authorized_keys = 'ssh-dss AAAAB3NzaC1kc3MAAACBAIeRYPgKLZvThg7foFrLzYUTD/mCAmaXjLl2WkAm9lmeQ0JoQIeujCzTYu2WsdP83ILOa0q83BXUXsUm3ylxnaQjvJ/WyWahy2ciyCccdndo+CXGcfascqqd+XGyIW/f9s/p4vNPtKrQcatvVDgonMfkNQzcsgpgUyeeMfbU50x7AAAAFQCtqfHY2ywqtZ7aasSFt8Mjbv6stQAAAIAc/oFyEyndEAaYpSUGYNxSJvd7MaUk3DY5qlmmvzkGS6rVV9f/ykullxpFKJEnAla4dcXc+WPTXdGDWvzON1mwMN+86znlxtKrZ44gy2RoAyUSXEk9fchRfJBo9lI+PrCPy8GaOuoWrLn63v9YuGovRq7sC+BgI0/Uu5LPvZQQFQAAAIBPOhzuSbz+28D01KxTxTqz6Hs75j+NAveCVsKRwJ5jQssjepNSta9MXKQ46Oxe3UdGFp8wcJgJLJYIW0C2EPWa3TVlnBzg5BNJSq/1HG2e8vD2uxF2Xx2q+9hYrSuNjv4paRYHZkLAJnspNp3Zl/LYU+TAZ81kEyEMXhUJrxe0oQ== edulix@edulix-laptop'
+$ssh_authorized_keys = 'ssh-dss ea:43:54:fa:90:3b:ab:74:7d:27:91:fa:7b:db:3b:fc root@iTodoRedGOB'
 
 # sentry conf
 
